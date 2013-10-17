6fd464ba3203f5ec9205a990cdd088deeba7b9ec
Ben <ben@zfben.com>
2013-10-17 15:20:47 +0800

    add --drop

--- lib/h2ocube_rails_tasks/tasks/mongodb.rb
+++ lib/h2ocube_rails_tasks/tasks/mongodb.rb
@@ -4,19 +4,19 @@ if File.exist? Rails.root.join('config/mongoid.yml')
     db = config['database']
     host = config['hosts'][0].split(':')[0]
     port = config['hosts'][0].split(':')[1]
-    
+
     backup = "mongodump --host #{host} --port #{port} --db #{db}"
     desc backup
     task :dump do
       zfben_rails_rake_system backup
     end
 
-    restore = "mongorestore --host #{host} --port #{port}"
+    restore = "mongorestore --host #{host} --port #{port} --drop"
     desc restore
     task :restore do
       zfben_rails_rake_system restore
     end
-    
+
     repair = "mongod --nohttpinterface --nojournal --port #{port} --bind_ip #{host} --dbpath #{Rails.root}/mongo --repair"
     desc repair
     task :repair do
