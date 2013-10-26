bc40dbcc59f317ee183517394e0f52a3a09844da
Ben <ben@zfben.com>
2013-10-27 07:59:15 +0800

    add postgresql

--- lib/h2ocube_rails_tasks/tasks/postgresql.rb
+++ lib/h2ocube_rails_tasks/tasks/postgresql.rb
@@ -1,30 +1,32 @@
 if File.exist? Rails.root.join('config/database.yml')
-  namespace :mysql do
+  namespace :postgresql do
     config = YAML.load(File.read(Rails.root.join('config/database.yml')))[Rails.env.to_s]
-    if config['adapter'] == 'mysql2'
+    if config['adapter'] == 'postgresql'
       db = config['database']
       host = config['host']
-      FileUtils.mkdir_p Rails.root.join('mysql')
+      user = config['username']
+      password = config['password']
+      FileUtils.mkdir_p Rails.root.join('postgresql')
 
-      backup = "mysqldump -h #{host} #{db} > #{Rails.root}/mysql/dump.sql"
+      backup = "pg_dump -h #{host} -d #{db} -U #{user} -f #{Rails.root}/postgresql/dump.sql"
       desc backup
       task :dump do
         zfben_rails_rake_system backup
       end
 
-      restore = "mysql -h #{host} #{db} < #{Rails.root}/mysql/dump.sql"
+      restore = "postgresql -h #{host} #{db} < #{Rails.root}/postgresql/dump.sql"
       desc restore
       task :restore => 'db:create' do
         zfben_rails_rake_system restore
       end
 
-      backup_all = "mysqldump -h #{host} --all-databases > #{Rails.root}/mysql/dump_all.sql"
+      backup_all = "pg_dump -h #{host} --all-databases > #{Rails.root}/postgresql/dump_all.sql"
       desc backup_all
       task :dump_all do
         zfben_rails_rake_system backup_all
       end
 
-      restore_all = "mysql -h #{host} < #{Rails.root}/mysql/dump_all.sql"
+      restore_all = "postgresql -h #{host} < #{Rails.root}/postgresql/dump_all.sql"
       desc restore_all
       task :restore_all => 'db:create' do
         zfben_rails_rake_system restore_all
