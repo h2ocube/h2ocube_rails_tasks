90a2ad513cfb14c263ed35e28dd69cdf22a4a8d9
Ben <ben@zfben.com>
2013-11-03 19:43:22 +0800

    v0.0.13

--- h2ocube_rails_tasks.gemspec
+++ h2ocube_rails_tasks.gemspec
@@ -3,7 +3,7 @@ $:.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = 'h2ocube_rails_tasks'
-  s.version     = '0.0.12'
+  s.version     = '0.0.13'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ['Ben']
   s.email       = ['ben@h2ocube.com']
--- lib/h2ocube_rails_tasks/tasks/backup.rb
+++ lib/h2ocube_rails_tasks/tasks/backup.rb
@@ -2,5 +2,6 @@ desc 'Backup all things'
 task :backup, [:to] do |task, args|
   Rake::Task['git:pull'].execute from: args[:to]
   Rake::Task['db:dump'].execute
-  Rake::Task['git:push'].execute comment: "Backup @ #{Time.now}", to: args[:to]
+  Rake::Task['git:commit'].execute comment: "Backup @ #{Time.now}"
+  Rake::Task['git:push'].execute to: args[:to]
 end
