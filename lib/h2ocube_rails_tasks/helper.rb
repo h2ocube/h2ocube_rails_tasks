4972b498cd2511b5d334971153aa69419b71232e
Ben <ben@zfben.com>
2014-03-13 12:15:41 -0400

    v0.0.14

--- lib/h2ocube_rails_tasks/helper.rb
+++ lib/h2ocube_rails_tasks/helper.rb
@@ -4,11 +4,11 @@ require 'yaml'
 
 def zfben_rails_rake_system cmd
   cmd = cmd
-  STDOUT.print (cmd + "\n").color(:green)
+  STDOUT.print Rainbow(cmd + "\n").color(:green)
   zfben_rails_rake_err unless system cmd
 end
 
 def zfben_rails_rake_err msg = 'Failed!'
-  STDERR.print (msg + "\n").color(:red)
+  STDERR.print Rainbow(msg + "\n").color(:red)
   exit!
 end
